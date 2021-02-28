68533eaaecefd9523f0144f815e8c3df6998c19b
Nam Nguyen <nam@icts.vn>
2021-02-28 16:04:17 +0700

    Create device group

--- iotupdate/models.py
+++ iotupdate/models.py
@@ -1,3 +1,25 @@
+import os
+import subprocess
+import datetime
+
+from django.utils.translation import ugettext as _
+
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(_('Created at'), auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(_('Updated at'), auto_now=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class DeviceGroup(models.BaseModel):
+    """
+    Description: Model Description
+    """
+    name = models.CharField(_('Name'), max_length=255)
+
+    class Meta:
+        db_table = 'device_groups'
