1ceb9e927b3c37fd4b1ffdf92494c3dab0eae4ae
Nam Nguyen <namnguyenvn@gmail.com>
2021-05-25 21:43:03 +0700

    set auto print temp

--- packages/hardware_state/hardware_state.py
+++ packages/hardware_state/hardware_state.py
@@ -4,9 +4,12 @@ import datetime
 
 
 def state():
-    current_time = datetime.datetime.now()
+    while(True):
+        current_time = datetime.datetime.now()
 
-    with open(r"/sys/class/thermal/thermal_zone0/temp") as file:
-        current_temp = file.readline()
+        with open(r"/sys/class/thermal/thermal_zone0/temp") as file:
+            current_temp = file.readline()
 
-    print(str(current_time) + " - " + str(float(current_temp) / 1000))
+        print(str(current_time) + " - " + str(float(current_temp) / 1000))
+
+        time.sleep(3)
